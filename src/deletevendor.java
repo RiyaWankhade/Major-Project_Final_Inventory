381c57f6008e0dbbcf5a64419780729f42046cf9
shrutibabhulkar <75988820+shrutibabhulkar@users.noreply.github.com>
2021-07-01 21:44:53 +0530

    Add files via upload

---
+++ src/deletevendor.java
@@ -0,0 +1,63 @@
+package master;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import generatedclass.connectionclass;
+
+public class deletevendor extends ActionSupport {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public String vendorId;
+
+	
+	public String getVendorId() {
+		return vendorId;
+	}
+	public void setVendorId(String vendorId) {
+		this.vendorId = vendorId;
+	}
+	
+	
+	public String ret="error";
+	public String execute() {
+		
+		HttpServletRequest request=ServletActionContext.getRequest();
+		HttpSession session=request.getSession();
+		
+		new connectionclass();
+		Connection con=connectionclass.getConnection();
+		String query="delete from vendor where id=?";
+		
+		try {
+			PreparedStatement pst=con.prepareStatement(query);
+			pst.setString(1,  vendorId);
+			pst.executeUpdate();
+			
+			
+			request.getSession().setAttribute("successmsg", "Data Deleted");
+			ret="success";
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ret="error";
+		}
+		
+		return ret;
+	}
+	
+	
+
+	
+}
